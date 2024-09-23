ad5ede384afedc2873dadf0c005fd2efd6c25be4
GingerBaddie <GingerBaddie>
2024-09-23 21:42:56 +0530

    new file:   01_basics/Conversions.js 	modified:   01_basics/hi.js

---
+++ 01_basics/Conversions.js
@@ -0,0 +1,36 @@
+// let score = "10";
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+
+// console.log(valueInNumber);
+
+
+// let score = "10abc";
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);     // NaN(not a number)
+
+let score = null;
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);     // NaN(not a number)
+
+
+// "10" => 10
+// "10abc" => NaN
+
+let isLoggedIn = 10;
+
+let booleanIsLoggedIn =  Boolean(isLoggedIn);  
+console.log(isLoggedIn);
+
+
--- 01_basics/hi.js
+++ 01_basics/hi.js
@@ -1,3 +1,4 @@
 console.log("hello")
 let age = 12;
+const name = "Abhi";
 console.log(typeof(age));
