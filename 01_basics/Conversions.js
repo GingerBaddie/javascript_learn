ae113e2c9e5defa3adc04f396c41fe8c9358fdf9
GingerBaddie <GingerBaddie>
2024-10-15 04:58:11 +0530

    modified:   01_basics/Conversions.js 	modified:   01_basics/hi.js 	01_basics/comparisons.js

--- 01_basics/Conversions.js
+++ 01_basics/Conversions.js
@@ -16,21 +16,45 @@
 // console.log(typeof(valueInNumber));
 // console.log(valueInNumber);     // NaN(not a number)
 
-let score = null;
+// let score = null;
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let valueInNumber = Number(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);     // NaN(not a number)
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);     // NaN(not a number)
 
 
 // "10" => 10
 // "10abc" => NaN
 
-let isLoggedIn = 10;
+// let isLoggedIn = 10;
 
-let booleanIsLoggedIn =  Boolean(isLoggedIn);  
-console.log(isLoggedIn);
+// let booleanIsLoggedIn =  Boolean(isLoggedIn);  
+// console.log(isLoggedIn);
+
+///////////////////////////////// Operations //////////////////////////////
 
 
+let value = 3;
+let negValue = -value;
+
+// console.log(value);
+// console.log(negValue);
+
+
+// console. log("1" + 2);
+// console. log(1 + "2");
+// console. log("1" +2 + 2);
+// console. log(1 + 2 +"2");
+
+// console.log( (3 +4) * 5 % 3);
+
+let a = "";
+let b = Boolean(a);
+console.log(b);     // Output => false
+
+
+let c = "";
+let d = Boolean(c);
+console.log(d);  // Output => true
