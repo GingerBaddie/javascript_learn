77041779bc42c3bf9c5c63f0043aa67878bf723f
GingerBaddie <GingerBaddie>
2024-10-22 22:39:56 +0530

    new file:   01_basics/numAndMaths.js 	modified:   01_basics/Conversions.js 	modified:   01_basics/hi.js

---
+++ 01_basics/numAndMaths.js
@@ -0,0 +1,44 @@
+// const score  = 20;
+// console.log(score);
+// console.log((score.toFixed(4)));
+
+// const balance = new Number(30);
+// console.log(balance);
+
+// console.log((balance.toString()));
+// console.log((balance.toString()).length);
+// console.log((balance.toFixed(1)));
+
+// console.log(balance.toLocaleString());
+
+// const num = 2347.7689;
+// console.log(num.toPrecision(10));
+
+// const hun = 100000000;
+// console.log(hun.toLocaleString('en-IN')); // add commas to read easily
+
+/////////////////////////////////////////// mAths //////////////////////////////////////
+
+console.log(Math);
+console.log(Math.abs(-4)); // gives absolute (mod) value
+console.log(Math.round(4.7));
+
+console.log(Math.ceil(2.7));
+console.log(Math.floor(3.2));
+console.log(Math.min(4, 7, 2, 9));
+console.log(Math.max(4, 7, 2, 9));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor((Math.random() * 10)) + 1);
+
+
+const min = 20;
+const max = 30;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
