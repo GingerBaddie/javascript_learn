adafe97283df33fa57826e7991418ad60624e869
GingerBaddie <GingerBaddie>
2025-01-28 20:49:53 +0530

    new file:   02_basics/01_arrays.js 	02_basics/02_array.js 	02_basics/03_objects.js 	02_basics/04_objects.js 	03_basics/ 	06_dom/

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHROS  = ["sm", "bv", "gnht"];
+// console.log(myArr);
+// console.log(myArr.length);
+// console.log(myHROS);
+// console.log(myArr[1]);
+
+// myArr.push(20);
+// myArr.push(26);
+// myArr.pop();
+// myArr.pop();
+// console.log(myArr);
+// myArr.unshift(30);
+// console.log(myArr);
+// myArr.shift();
+
+// console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+// console.log(myArr);
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
+
+
+
